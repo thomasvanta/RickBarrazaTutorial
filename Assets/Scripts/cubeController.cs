3caef725978e0175007ffd6d256b5ea7d8ae8c2d
Thomas Van Ta <fanta_e@hotmail.com>
2017-12-20 16:45:15 +0100

    leaving PostPro out

---
+++ Assets/Scripts/cubeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeController : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinSpeed = Vector3.zero;
+	Vector3 spinAxis = Vector3.up;
+
+	// Use this for initialization
+	void Start () {
+		spinSpeed = spinSpeed + new Vector3 (Random.value, Random.value, Random.value);
+		spinAxis.x = Random.Range (-0.1f, 0.1f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (spinSpeed);
+		this.transform.RotateAround (Vector3.zero, spinAxis, rotateSpeed);
+		
+	}
+
+	public void setSize(float _size){
+		this.transform.localScale = new Vector3 (_size, _size, _size);
+	}
+}
---
+++ Assets/Scripts/sketch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sketch : MonoBehaviour {
+
+	public GameObject _prefab;
+
+	public int totalCubes = 6;
+
+	public float maxSize = 0.5f;
+
+	public float minSize = 0.1f;
+
+	public float maxSpeed = 1.0f;
+
+	public Vector3 positionOffset = Vector3.zero;
+
+	public float totalDistance = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < totalCubes; i++) {
+
+			float percent = i / (float)totalCubes;
+
+			float sin = Mathf.Sin (percent * Mathf.PI/2);
+
+			float x = positionOffset.x;
+			float y = positionOffset.y;
+			float z = positionOffset.z + (sin * totalDistance);
+
+			var newPrefab = (GameObject)Instantiate (_prefab, new Vector3(x, y, z), Quaternion.identity);
+			newPrefab.GetComponent<cubeController> ().setSize (maxSize * (1.0f - percent) + minSize);
+			newPrefab.GetComponent<cubeController> ().rotateSpeed = maxSpeed * (1.0f - percent);//Random.value;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
